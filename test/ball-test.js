dd98d41dc200992220ad382df4580275f1d56375
Jason Pilz <jasonpilz@gmail.com>
2016-02-11 13:58:38 -0700

    big spike to expirement with ideas

---
+++ test/ball-test.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const Ball   = require('../lib/ball');
+
+describe('Ball', function() {
+  context('with default attributes', function() {
+
+    let ball = new Ball();
+
+    it('should assign an x coordinate', function() {
+      assert.equal(ball.x, 0);
+    });
+
+    it('should assign a y coordinate', function() {
+      assert.equal(ball.y, 0);
+    });
+
+    it('should assign a x_speed', function(){
+      assert.equal(ball.x_speed, 3);
+    });
+
+    it('should assign a y_speed', function(){
+      assert.equal(ball.y_speed, 0);
+    });
+
+    it('should assign a default radius', function(){
+      assert.equal(ball.radius, 10);
+    });
+  });
+
+});
---
+++ test/index.js
@@ -0,0 +1,5 @@
+const chai = require('chai');
+const assert = chai.assert;
+require('./ball-test')
+require('./paddle-test')
+require('./game-test')
